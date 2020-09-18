1eba2ba416e5133a74ad53f07d1f60f9b53b2763
Xizafren <m-ett@hotmail.se>
2020-09-18 14:23:08 +0200

    Added variable for questionslist and tried it with log

--- app/src/main/java/com/example/projectapp1/QuestionsActivity.kt
+++ app/src/main/java/com/example/projectapp1/QuestionsActivity.kt
@@ -2,11 +2,15 @@ package com.example.projectapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions Size", "${questionsList.size}")
 
 
     }
